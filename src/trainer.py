358ddf2ef892c6808e6bfbe3631d7ac782316271
Omkar-Ranadive <omkar.r1997@gmail.com>
2022-03-22 02:34:34 -0700

    Save more info in checkpoints

--- src/trainer.py
+++ src/trainer.py
@@ -88,6 +88,9 @@ parser.add_argument('--save_batch_targets', action='store_true', help='Save batc
 parser.add_argument("--use_ws", action='store_true', help='If true, weighted sampler is used')
 
 best_acc1 = 0
+best_acc5 = 0 
+train_acc1 = 0 
+train_acc5 = 0 
 
 def main():
     args = parser.parse_args()
@@ -139,6 +142,10 @@ def main():
 
 def main_worker(gpu, ngpus_per_node, args):
     global best_acc1
+    global best_acc5 
+    global train_acc1 
+    global train_acc5 
+
     args.gpu = gpu
 
     if args.gpu is not None:
@@ -211,9 +218,19 @@ def main_worker(gpu, ngpus_per_node, args):
                 checkpoint = torch.load(args.resume, map_location=loc)
             args.start_epoch = checkpoint['epoch']
             best_acc1 = checkpoint['best_acc1']
+            # Make sure the remaining keys exists 
+            if 'best_acc5' in checkpoint: 
+                best_acc5 = checkpoint['best_acc5']
+                train_acc5 = checkpoint['train_acc5']
+                train_acc1 = checkpoint['train_acc1']
+            else: 
+                best_acc5, train_acc5, train_acc1 = 0, 0, 0 
+
             if args.gpu is not None:
                 # best_acc1 may be from a checkpoint from a different GPU
                 best_acc1 = best_acc1.to(args.gpu)
+                best_acc5, train_acc5, train_acc1 = best_acc5.to(args.gpu), train_acc5.to(args.gpu), train_acc1.to(args.gpu)
+
             model.load_state_dict(checkpoint['state_dict'])
             optimizer.load_state_dict(checkpoint['optimizer'])
             print("=> loaded checkpoint '{}' (epoch {})"
@@ -285,14 +302,17 @@ def main_worker(gpu, ngpus_per_node, args):
         adjust_learning_rate(optimizer, epoch, args)
 
         # train for one epoch
-        train(train_loader, model, criterion, optimizer, epoch, args)
+        train_acc1, train_acc5 = train(train_loader, model, criterion, optimizer, epoch, args)
 
         # evaluate on validation set
-        acc1 = validate(val_loader, model, criterion, args)
+        acc1, acc5 = validate(val_loader, model, criterion, args)
 
         # remember best acc@1 and save checkpoint
         is_best = acc1 > best_acc1
         best_acc1 = max(acc1, best_acc1)
+        best_acc5 = max(acc5, best_acc5)
+        # train_acc1 = max(train_acc1, acc_t1)
+        # train_acc5 = max(train_acc5, acc_t5)
 
         if not args.multiprocessing_distributed or (args.multiprocessing_distributed
                 and args.rank % ngpus_per_node == 0):
@@ -301,6 +321,9 @@ def main_worker(gpu, ngpus_per_node, args):
                 'arch': args.arch,
                 'state_dict': model.state_dict(),
                 'best_acc1': best_acc1,
+                'best_acc5': best_acc5, 
+                'train_acc1': train_acc1,
+                'train_acc5': train_acc5,
                 'optimizer' : optimizer.state_dict(),
             }, is_best, filename='checkpoint_e{}.pth.tar'.format(epoch))
 
@@ -374,6 +397,8 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
     
     if args.save_batch_targets: 
         torch.save(batch_targets, EXP_DIR / 'bt_e{}.pt'.format(epoch))
+
+    return top1.avg, top5.avg 
         
 
 def validate(val_loader, model, criterion, args):
@@ -416,7 +441,7 @@ def validate(val_loader, model, criterion, args):
 
         progress.display_summary()
 
-    return top1.avg
+    return top1.avg, top5.avg 
 
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
